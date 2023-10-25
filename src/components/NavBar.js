a299c295af13557fd31caf4025be11af7fc81e06
jeffmanhart <jeffreymanhart@gmail.com>
2023-10-25 12:38:58 -0600

    got the iterating of react components thing going

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return ( <nav>
+    {links.map((e)=> {
+    return <a key={e} href={'#'+ e} >{e}</a>}
+    )}
+  </nav>)
 }
 
 export default NavBar;
